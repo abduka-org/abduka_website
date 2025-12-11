114137f791f87543ac4539f0983a52d69a285158
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-12-11 15:00:09 -0300

    ADD: shadcn and react bits components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
+export type TActiveScreen = "home";
+
 function App() {
-  return <></>;
+  const [activeScreen, setActiveScreen] = useState<TActiveScreen>("home");
+  const [isDarkTheme, setIsDarkTheme] = useState(
+    document.body.classList.contains("dark")
+  );
+
+  useEffect(() => {
+    const html = document.body;
+
+    const observer = new MutationObserver(() => {
+      setIsDarkTheme(html.classList.contains("dark"));
+    });
+
+    observer.observe(html, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <main className="flex flex-col min-w-full overflow-x-hidden">
+      <Navbar activeScreen={activeScreen} isDarkTheme={isDarkTheme} />
+
+      <Home setActiveScreen={setActiveScreen} />
+    </main>
+  );
 }
 
 export default App;
