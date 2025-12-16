edeedd95ba6ce319e15b95d22a11de66b0a0c5b2
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-12-16 13:58:35 -0300

    FEAT: navbar a11y

--- src/components/Navbar/Modal.tsx
+++ src/components/Navbar/Modal.tsx
@@ -12,39 +12,43 @@ interface IModalProps {
 
 const Modal = ({ activeScreen }: IModalProps) => {
   return (
-    <div
+    <nav
       id="mobile-menu"
       className="absolute top-16 right-4 z-50"
-      role="menu"
-      aria-label={"Navegação mobile"}
+      aria-label="Navegação mobile"
     >
       <Card>
-        <CardContent className="flex flex-col">
-          {navbarData.links.map(({ id, text, icon: Icon, action }) => (
-            <li key={id} className="list-none">
-              <button
-                className={`relative flex flex-row items-center gap-x-4 w-full rounded-md hover:text-primary transition-all duration-200 cursor-pointer px-6 py-4 pr-8 text-base text-nowrap
+        <CardContent>
+          <ul className="flex flex-col">
+            {navbarData.links.map(({ id, text, icon: Icon, action }) => (
+              <li key={id} className="list-none">
+                <button
+                  className={`relative flex flex-row items-center gap-x-4 w-full rounded-md hover:text-primary transition-all duration-200 cursor-pointer px-6 py-4 pr-8 text-base text-nowrap
                     ${
                       activeScreen === id
                         ? "bg-primary/10 font-semibold text-primary"
                         : "font-medium text-primary-text"
                     }`}
-                onClick={action}
-                role="menuitem"
-              >
-                <Icon size={16} aria-hidden />
+                  onClick={action}
+                  aria-current={activeScreen === id ? "page" : undefined}
+                >
+                  <Icon size={16} aria-hidden />
 
-                {text}
+                  {text}
 
-                {activeScreen === id && (
-                  <BadgeCheck className="absolute right-4 w-4 h-4 stroke-primary rounded-full" />
-                )}
-              </button>
-            </li>
-          ))}
+                  {activeScreen === id && (
+                    <BadgeCheck
+                      className="absolute right-4 w-4 h-4 stroke-primary rounded-full"
+                      aria-hidden
+                    />
+                  )}
+                </button>
+              </li>
+            ))}
+          </ul>
         </CardContent>
       </Card>
-    </div>
+    </nav>
   );
 };
 
